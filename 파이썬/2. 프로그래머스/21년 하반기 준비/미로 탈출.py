f1784b7ebf3cc24f8cc233e2ba45343e50b0f004
brrgi <48500985+brrgi@users.noreply.github.com>
2021-08-29 15:48:54 +0900

    20210829

---
+++ 파이썬/2. 프로그래머스/21년 하반기 준비/미로 탈출.py
@@ -0,0 +1,11 @@
+def solution(n, start, end, roads, traps):
+    answer = 0
+    return answer
+
+
+n=3
+start=1
+end=3
+roads=[[1, 2, 2], [3, 2, 3]]
+traps=[2]
+print(solution(n, start, end, roads, traps))
